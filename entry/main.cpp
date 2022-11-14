12813714c86d014f74d95eaf8f71b71deb439de9
Shreyas Khati <khatishreyas@gmail.com>
2022-11-13 21:45:53 -0600

    Added data files

--- entry/main.cpp
+++ entry/main.cpp
@@ -1,11 +1,8 @@
 #include "Graph.h"
 
 int main(int argc, char** argv) {
-
     //DO DATA PROCESSING HERE, FEED IN PATH TO CSV FILE THROUGH TERMINAL
-
     //READ FROM CSV FILE BY CALLING extractCSVData Method
-
     //TODO Shreyas, Shaarav
 
     return 0;

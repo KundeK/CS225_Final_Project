d9f9b6ac4087aa471c67b3e427a7c8a5863944ea
aadarshhegde <ahegd2@illinois.edu>
2022-12-08 04:53:21 +0000

    Added distance calculation helper methods in main.cpp

--- entry/main.cpp
+++ entry/main.cpp
@@ -118,10 +118,32 @@ int main(int argc, char** argv) {
 }
 
 
-//Helper function for calculating relative distance between two airports.
-//Will require longitude and latitude coordinates of two airports. 
-int relative_Distance(){ 
-    
-    return 0;
-    
+long double toRadians(long double degree) {
+    long double one_deg = (M_PI) / 180;
+    return (one_deg * degree);
+}
+
+double getEdgeWeight(long double lat1, long double long1, long double lat2, long double long2) {
+    lat1 = toRadians(lat1);
+    long1 = toRadians(long1);
+    lat2 = toRadians(lat2);
+    long2 = toRadians(long2);
+     
+    // Haversine Formula, taken from wikipedia
+    long double dlong = long2 - long1;
+    long double dlat = lat2 - lat1;
+ 
+    long double result = std::pow(std::sin(dlat / 2), 2) +
+                          std::cos(lat1) * std::cos(lat2) *
+                          std::pow(std::sin(dlong / 2), 2);
+ 
+    result = 2 * asin(sqrt(result));
+ 
+    // Radius of Earth in miles = 3956
+    long double radii = 3956;
+     
+    // Calculate the result
+    result = result * radii;
+ 
+    return result;
 }

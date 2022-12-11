90d2265932b301ba5dca56243496c33c5b5832dd
Krushankb <krushankb@gmail.com>
2022-12-11 02:18:31 +0000

    Set up test, but buggy

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -5,19 +5,20 @@
 //Graph Test Cases - Aadarsh
 
 TEST_CASE("Construct Graph Simple", "[weight=1][part=Graph]") {
-
+    // int v = 0;
+    REQUIRE(0 == 0);
 }
 
-TEST_CASE("Construct Graph Complex", "[weight=4][part=Graph]") {
+// TEST_CASE("Construct Graph Complex", "[weight=4][part=Graph]") {
     
-}
+// }
 
-TEST_CASE("Construct Graph Edge Case 1", "[weight=4][part=Graph]") {
+// TEST_CASE("Construct Graph Edge Case 1", "[weight=4][part=Graph]") {
     
-}
+// }
 
-TEST_CASE("Construct Graph Edge Case 2", "[weight=4][part=Graph]") {
+// TEST_CASE("Construct Graph Edge Case 2", "[weight=4][part=Graph]") {
     
-}
+// }
 
 //ADD UNIT TEST CASES FOR OTHER PARTS OF CODE
